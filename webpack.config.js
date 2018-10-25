4607113935e448b453c9da8f7a8883ea190e80d4
~RhysSharratt <xbyatomz@gmail.com>
2018-10-25 23:06:29 +0100

    Got Index working

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path')
+var webpack = require('webpack')
+var htmlWebpackPlugin = require('html-webpack-plugin')
+
+var html = new htmlWebpackPlugin();
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: "/dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-webpack-plugin'
+            }
+        ]
+    },
+    plugins: [
+        html
+    ]
+}
